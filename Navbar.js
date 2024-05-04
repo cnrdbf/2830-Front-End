4a66a8c9ae7c8a40f7300401647a91ac121d82a6
cnrdbf <75334721+cnrdbf@users.noreply.github.com>
2024-05-04 18:47:05 -0500

    Update Navbar.js

--- Navbar.js
+++ Navbar.js
@@ -6,22 +6,8 @@ function Navbar() {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
         <Link className="navbar-brand" to="/">Event Planner</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav me-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/create-event">Create Event</Link>
-            </li>
-          </ul>
           <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/login">Login</Link>
-            </li>
           </ul>
         </div>
       </div>
@@ -30,3 +16,4 @@ function Navbar() {
 }
 
 export default Navbar;
+
